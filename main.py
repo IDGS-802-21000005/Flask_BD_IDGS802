6774194b8b0282a1e0ab07eee2924ef8b0b8bcb3
IDGS-802-21000005 <CarlosIRBello@gmail.com>
2024-02-29 21:06:58 -0600

    CIRB 29/02/2024 ABC Completo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,render_template, Response
+from flask import Flask, request,render_template, Response, redirect, url_for
 
 from flask_wtf.csrf import CSRFProtect
 from flask import g
@@ -22,7 +22,7 @@ def index():
         #insert into alumnos values()
         db.session.add(alum)
         db.session.commit()
-        
+        return redirect(url_for("ABC_Completo"))
     return render_template("index.html", form=alumnos_form)
 
 @app.route("/ABC_Completo", methods=["GET","POST"])
@@ -52,7 +52,46 @@ def alumnos():
         print(f"nombre:{correo}")
     return render_template("alumnos.html", form=alumnos_form, nom=nom, apa=apaterno, ama=amaterno, edad=edad, correo=correo)
 
-    
+@app.route("/eliminar",methods=["GET","POST"])
+def eliminar():
+    alumnos_form=forms.UserForm3(request.form)
+    if request.method == "GET":
+        id=request.args.get("id")
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alumnos_form.id.data=alum1.id
+        alumnos_form.nombre.data=alum1.nombre
+        alumnos_form.apaterno.data=alum1.apaterno
+        alumnos_form.email.data=alum1.email
+    if request.method == "POST":
+        id=alumnos_form.id.data
+        alum=Alumnos.query.get(id)
+        db.session.delete(alum)
+        db.session.commit()
+        return redirect(url_for("ABC_Completo"))
+    return render_template("eliminar.html", form=alumnos_form)
+
+@app.route("/modificar",methods=["GET","POST"])
+def modificar():
+    alumnos_form=forms.UserForm3(request.form)
+    if request.method == "GET":
+        id=request.args.get("id")
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alumnos_form.id.data=alum1.id
+        alumnos_form.nombre.data=alum1.nombre
+        alumnos_form.apaterno.data=alum1.apaterno
+        alumnos_form.email.data=alum1.email
+    if request.method == "POST":
+        id=alumnos_form.id.data
+        alum=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum.nombre=alumnos_form.nombre.data
+        alum.apaterno=alumnos_form.apaterno.data
+        alum.email=alumnos_form.email.data
+        db.session.add(alum)
+        db.session.commit()
+        
+        return redirect(url_for("ABC_Completo"))
+    return render_template("modificar.html", form=alumnos_form)
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"),404
